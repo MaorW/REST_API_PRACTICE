1b3f2674a4835bf4ed4eb3dd605e356218e093ef
maor weiss <maorweissb@gmail.com>
2022-12-07 21:07:10 +0200

    Hothi's REST

---
+++ CLASS/main.py
@@ -0,0 +1,53 @@
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+
+cars = [{'id': 1, 'make': 'audi', 'model': 'A7', 'year': 2022},
+        {'id': 2, 'make': 'bmw', 'model': 'x6', 'year': 2021},
+        {'id': 3, 'make': 'volvo', 'model': 's90', 'year': 2012},
+        {'id': 4, 'make': 'mazda', 'model': '3', 'year': 2015},
+        {'id': 5, 'make': 'skoda', 'model': 'Octatvia', 'year': 2014}]
+
+# print(cars[1]) # {'id': 2, 'make': 'bmw', 'model': 'x6', 'year': 2021}
+
+@app.route('/', methods=['GET'])
+def home():
+    return '<h1 style="color:red;"> I love DevOps </h1> '
+
+
+@app.route('/cars', methods=['GET'])
+def getCars():
+    return json.dumps(cars)
+
+
+@app.route('/cars/<int:id>', methods=['GET'])
+def getCar(id):
+    return json.dumps( cars[id - 1] )
+
+
+@app.route('/cars/<int:id>', methods=['DELETE'])
+def deleteCar(id):
+    for car in cars:
+        if car['id'] == id:
+            cars.remove(car)
+    return json.dumps( cars )
+
+
+@app.route('/cars', methods=['POST'])
+def addCar():
+      car = request.json
+      cars.append(car)
+      return "sir yes sir"
+
+
+@app.route('/cars/<int:id>', methods=['PUT'])
+def updateCar(id):
+      newCar = request.json
+      for car in cars:
+            if car['id']==id:
+                  cars.remove(car)
+      cars.append(newCar)
+      return 'updated',299
+
+app.run() # http://127.0.0.1:5000/cars -> Json
